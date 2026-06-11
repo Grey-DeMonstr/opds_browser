ec0bf31c19bb0f9706c36078212ae2f0e6e1f404
Sergei Ivanov <greydemonster@gmail.com>
2026-06-11 20:38:11 +0200

    Configure Android: minSdk 29, applicationId monster.greyde.opds_browser, OPDS Browser label

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.opds_browser"
+    namespace = "monster.greyde.opds_browser"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,10 +16,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.opds_browser"
+        applicationId = "monster.greyde.opds_browser"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 29
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
