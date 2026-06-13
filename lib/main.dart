4331d0cdb7f1fe85d2ac141126b1897b7e2092ab
Sergei Ivanov <greydemonster@gmail.com>
2026-06-14 00:02:36 +0200

    Initialize MediaStorage

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:media_store_plus/media_store_plus.dart';
 import 'package:opds_browser/app.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
+  }
+  if (Platform.isAndroid) {
+    await MediaStore.ensureInitialized();
   }
   runApp(const ProviderScope(child: OpdsBrowserApp()));
 }
